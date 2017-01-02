739946c42eca3423b86bb65b3a0624d93b4849b5
forsona <2317216477@qq.com>
2017-01-02 18:12:30 +0800

    init

---
+++ config/Templates/Scripting/PHPClass.php
@@ -0,0 +1,21 @@
+<?php
+
+<#if namespace?? && namespace?length &gt; 0>
+namespace ${namespace};
+</#if>
+
+/**
+ * Description
+ *
+ * @namespace ${namespace}
+ * @filename ${nameAndExt}
+ * @encoding ${encoding}
+ * @author ${author}
+ * @link ${link}
+ * @copyright ${copyright}
+ * @datetime ${date} ${time}
+ * @version ${version}
+ */
+class ${name} {
+//put your code here
+}
